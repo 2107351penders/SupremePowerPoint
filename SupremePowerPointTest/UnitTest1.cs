47ad8347efd9102ba2c686e4e3928aa07b75512f
Tim Penders <2107351penders@zuyd.nl>
2022-12-09 22:45:30 +0100

    Eerste commit
    Voeg hoofd project met klasses uit ontwerp en een leeg NUnit project
    toe. Voeg tevens .gitignore voor Visual Studio toe en een lege README.

---
+++ SupremePowerPointTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SupremePowerPointTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
