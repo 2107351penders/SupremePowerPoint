f9ba952433f0e8b20a96b2df7c737518ab719f07
Tim Penders <2107351penders@zuyd.nl>
2022-12-19 21:37:13 +0100

    Add classes for file input
    Add the classes necessary to implement reading Presentatie files

---
+++ SupremePowerPointApp/IPresentatieReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupremePowerPointApp
+{
+    /* Een IPresentatieReader moet een bestand met een presentatie erin kunnen openen.
+     * De dia's van deze presentatie worden dan als een IEnumerable beschikbaar gemaakt
+     * zodat we er met foreach over heen kunnen lopen in de PresentatieBouwer
+     */
+    internal interface IPresentatieReader : IEnumerable
+    {
+        public bool openPresentatie(String presentatieBestand);
+    }
+}
---
+++ SupremePowerPointApp/JsonPresentatieReader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupremePowerPointApp
+{
+    internal class JsonPresentatieReader : IPresentatieReader
+    {
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool openPresentatie(string presentatieBestand)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
