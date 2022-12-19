7e127a147382f1e8078b1699a863c7a9324b760e
Tim Penders <2107351penders@zuyd.nl>
2022-12-19 21:36:10 +0100

    Add implementation to Presentatie class

--- SupremePowerPointApp/Presentatie.cs
+++ SupremePowerPointApp/Presentatie.cs
@@ -8,5 +8,19 @@ namespace SupremePowerPointApp
 {
     internal class Presentatie
     {
+        private List<Dia> diaLijst = new List<Dia>();
+        public int zichtbareDia { get; private set; } = 0;
+
+        public bool displayDia(int diaNummer)
+        {
+            zichtbareDia= diaNummer;
+            // Display dia. Return true wanneer dit lukt
+            return true;
+        }
+
+        public void addDia(Dia dia)
+        {
+            diaLijst.Add(dia);
+        }
     }
 }
